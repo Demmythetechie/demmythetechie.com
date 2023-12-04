5ca6a65273c74b13f617730889de2f39b7e7499c
Naheem Okunade <109427907+Demmythetechie@users.noreply.github.com>
2023-12-04 15:02:12 +0000

    updated

--- src/components/Home/intro.js
+++ src/components/Home/intro.js
@@ -1,10 +1,11 @@
 function Intro () {
     return (
-        <div className="flex flex-row justify-center h-[80%] border border-white w-full pl-[8vh] pr-[8vh]">
-            <div className="flex justify-center items-center w-[600px] border border-white ">
-                <div className="flex items-end h-[238px] w-[210px]">
-                    <img className="rounded-[200px]" src="Naheem.jpg" alt=""/>
-                </div>
+        <div className="flex flex-row justify-between h-[80%] border border-white w-full pl-[8vh] pr-[8vh]">
+            <div className="w-[600px] border">
+
+            </div>
+            <div className="w-[600px] border flex items-center justify-center">
+                <img className="rounded-[50%] w-[339px] h-[335px]" src="Naheem.png" alt=""/>
             </div>
         </div>
     );
