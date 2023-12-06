7c5e615237726bd01844acfb2513e18bb9eacb50
Demmythetechie <demmythetechie@gmail.com>
2023-12-06 06:52:43 +0100

    updated

--- src/components/Home/intro.js
+++ src/components/Home/intro.js
@@ -1,11 +1,16 @@
 function Intro () {
     return (
-        <div className="flex flex-row justify-between h-[80%] border border-white w-full pl-[8vh] pr-[8vh]">
-            <div className="w-[600px] border">
-
+        <div className="flex flex-row justify-center h-[90%] w-full pl-[8vh] pr-[8vh]">
+            <div className="flex flex-col justify-center items-center w-[600px]">
+                <img className="rounded-[50%] w-[237px] h-[235px]" src="Naheem.png" alt=""/>
+                <p className="text-white text-[37px] font-Ubuntu_Medium">Naheem Okunade</p>
+                <p className="text-white text-[20px] font-Ubuntu_Light">A Full-Stack web Developer</p>
             </div>
-            <div className="w-[600px] border flex items-center justify-center">
-                <img className="rounded-[50%] w-[339px] h-[335px]" src="Naheem.png" alt=""/>
+            <div className="absolute flex flex-col justify-end gap-y-[40px] w-[20px] z-2 bottom-[70px] right-[15px]">
+                <img className="w-[20px] h-[20px]" src="github.png" alt=""/>
+                <img className="w-[20px] h-[20px]" src="linkedin.png" alt=""/>
+                <img className="w-[20px] h-[20px]" src="twitter.png" alt=""/>
+                <div className="h-[50px] w-[0px] border border-white"></div>
             </div>
         </div>
     );
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 function Header(){
     return (
-        <div className="flex flex-row items-end justify-start gap-x-[460px] h-[8%] w-full pl-[8vh] pr-[8vh]">
+        <div className="flex flex-row items-end justify-between gap-x-[460px] h-[8%] w-full pl-[8vh] pr-[8vh]">
             <div>
                 <img className="w-[30px] h-[30px]" src="logo_white.png" alt=""/>
             </div>
-            <nav className="flex flex-row w-[350px] justify-between">
-                <Link className="text-white font-Ubuntu_Medium" to="/portfolio">Porfolio</Link>
+            <nav className="flex flex-row gap-x-[65px] justify-between">
+                <Link className="text-white font-Ubuntu_Medium" to="/about">Home</Link>
+                <Link className="text-white font-Ubuntu_Medium" to="/portfolio">Works</Link>
                 <Link className="text-white font-Ubuntu_Medium" to="/resume">Resume</Link>
-                <Link className="text-white font-Ubuntu_Medium" to="/about">About</Link>
                 <Link className="text-white font-Ubuntu_Medium" to="/about">Contact</Link>
             </nav>
         </div>
