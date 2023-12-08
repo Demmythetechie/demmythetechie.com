4a06cebb58355285f3a9e83e612465d8cc6f0680
Demmythetechie <demmythetechie@gmail.com>
2023-12-08 08:23:26 +0100

    updated

--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -1,15 +1,17 @@
 function Service () {
     return (
         <div className="flex flex-col h-[90vh] border border-white">
-            <section className="w-[100%] h-[50%] border border-white">
-                <div>
-                    <p className="text-white text-[17px] h-[23px] font-Ubuntu_Regular border border-white">Services</p>
-                    <p className="text-white text-[35px] h-[48px] font-Ubuntu_Medium border border-white box-border">Language</p>
+            <section className="w-[100%] h-[50%]">
+                <div className="flex flex-col justify-start">
+                    <p className="text-white text-[17px] h-[20px] font-Ubuntu_Regular">Services</p>
+                    <p className="text-white text-[35px] font-Ubuntu_Medium">Language</p>
                     <svg x="0px" y="0px" width="170px" height="5px">
                         <path fill="#fff" stroke-width="5" stroke="#fff" d="M0 0 l1120 0"/>
                     </svg>
                 </div>
-
+                <div className="flex flex-row">
+                    
+                </div>
             </section>
             <section className="w-[100%] h-[50%] border border-white">
 
