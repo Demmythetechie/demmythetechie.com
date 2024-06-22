86189e68cbaddae74106b3c1b72e16c117550a6b
Demmythetechie <demmythetechie@gmail.com>
2024-06-22 21:07:25 +0100

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ function Header(){
 
     return (
         <div className="max-[800px]:absolute max-[800px]:z-[1] max-[800px]:top-[0%] flex flex-row items-center justify-between h-[10vh] w-full pl-[8vh] pr-[8vh] max-[800px]:justify-between max-[800px]:pr-[0vh] max-[800px]:pl-[0vh]">
-            <a href="http://localhost:3000"><img className="w-[30px] h-[30px] max-[800px]:ml-[4vh]" src="logo_white.png" alt=""/></a>
+            <a href="https://demmythetechie-com-naheem-okunades-projects.vercel.app/"><img className="w-[30px] h-[30px] max-[800px]:ml-[4vh]" src="logo_white.png" alt=""/></a>
             <nav className="w-[45%] flex flex-row justify-between items-center max-[1024px]:w-[38%] max-[1024px]:ml-[5%] max-[800px]:hidden">
                 {navigation.map((nav)=>(
                     <Link onClick={()=>clck(nav.page)} className={`text-white font-Ubuntu_Medium text-[1vw] max-[1024px]:text-[1.3vw] ${(nav.page === option) ? 'text-opacity-[0.4]' : ''}`} to={nav.goto}>{nav.page}</Link>
@@ -29,11 +29,11 @@ function Header(){
                 <div className="max-[1024px]:hidden flex justify-center text-[1vw] items-center w-[90px] h-[28px] text-black font-Ubuntu_Medium bg-[#fff] rounded-[20px] text-center">Contact</div>
             </nav>
             <div className="max-[800px]:hidden hidden max-[1024px]:flex justify-center text-[1.1vw] max-[1024px]:text-[1.3vw] items-center w-[90px] h-[28px] text-black font-Ubuntu_Medium bg-[#fff] rounded-[20px] text-center">Contact</div>
-            <div onClick={nav} className="max-[800px]:mr-[4vh] min-[801px]:hidden max-[800px]:flex items-center justify-center w-[4%] h-[45%] max-[480px]:w-[7%]">
-                <svg fill="#fff" viewBox="0 0 100 100">
-                    <line x1="0" y1="25" x2="100" y2="25" stroke="#fff" strokeWidth="7" strokeLinecap="round"/>
-                    <line x1="0" y1="50" x2="70" y2="50" stroke="#fff" strokeWidth="7" strokeLinecap="round"/>
-                    <line x1="0" y1="75" x2="90" y2="75" stroke="#fff" strokeWidth="7" strokeLinecap="round"/>
+            <div onClick={nav} className="max-[800px]:mr-[5vw] min-[801px]:hidden max-[800px]:flex items-center justify-center w-[4%] h-[45%] max-[480px]:w-[7%]">
+                <svg viewBox="0 0 100 100">
+                    <line x1="0" y1="25" x2="100" y2="25" stroke="#ffffff" strokeWidth="7" strokeLinecap="round"/>
+                    <line x1="0" y1="50" x2="70" y2="50" stroke="#ffffff" strokeWidth="7" strokeLinecap="round"/>
+                    <line x1="0" y1="75" x2="90" y2="75" stroke="#ffffff" strokeWidth="7" strokeLinecap="round"/>
                 </svg>
             </div>
             <div className={(bool) ? "absolute min-[801px]:hidden max-[800px]:flex flex-col justify-around items-center w-[100vw] border-t-[1px] border-b-[1px] border-[#e8e8e80d] backdrop-blur-[10px] backdrop-brightness-[0.2] h-[45vh] top-[90%] pl-[2%] pr-[2%] pt-[1%] pb-[1%]" : "hidden"}>
